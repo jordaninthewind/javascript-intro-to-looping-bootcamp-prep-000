8f021688ff5ca000a23e796ba9790cc3e944c825
jordaninthewind <jordan.kline@gmail.com>
2017-09-15 17:45:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array, num) {
   var num2 = num + 25;
-  for (var i = 0; i < num2; i++) {
+  for (var i = 0; i <= num2; i++) {
     array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
   }
 return array;
